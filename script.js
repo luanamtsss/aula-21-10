a196769d8c545951c84631169db59b910d445ba9
gholyra <luanam.mts25@gmail.com>
2024-10-21 08:46:44 -0300

    slaaa

---
+++ script.js
@@ -0,0 +1,46 @@
+// const numeros = [2,4,7,13,6,8]
+// let numerosPares = []
+
+// numeros.forEach(function(pegaCadaNumero){
+//     if (pegaCadaNumero % 2 === 0) {
+//         numerosPares.push(pegaCadaNumero)
+//     }
+// })
+
+// console.log(numerosPares)
+
+
+
+
+// let nomes = ["João", "Maria", "Carlos", "Ana"];
+
+// nomes.forEach(function(pegaNomes){
+//     console.log(`Sr(a) ${pegaNomes}`)
+// })
+
+
+
+
+
+// let numeros = [4, 11, 8, 15, 23, 7, 2];
+// contador = 0
+
+// numeros.forEach(function(pegaNumeros) {
+//     if (pegaNumeros > 10) {
+//         contador++
+//     }
+// })
+// console.log(contador)
+
+
+
+
+
+
+// let frase = "Memphis Depay é Brabo";
+// let palavras = frase.split('')
+// contador = 0 
+
+// palavras.forEach(function(pegaPalavras){
+//     contador++
+// })
